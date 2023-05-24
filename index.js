5e476435b8b91bf0091cb62021673762ad70e4aa
Angeth-Herjok <angethherjok@gmail.com>
2023-05-24 16:24:29 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+document.body.style.backgroundColor="silver"
+
+
+document.getElementById('title').style.color='green'
+
+let h3Tags=document.getElementById
+
+
+
+let li=document.createElement('li')
+li.innerHTML='Banana';
+document.getElementById('fruList').appendChild(li)
+
+
+// let li=document.createElement('li')
+// li.innerHTML='Spinach';
+// document.getElementById('vegList').appendChild(li)
+
